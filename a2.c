5d5696ca771ecf30f8260e8c04477949697e76f2
Alex <andres.al.campos@gmail.com>
2018-06-13 01:52:28 +0000

    added more stuff

--- a2.c
+++ a2.c
@@ -1,45 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	printf("Welcome to the super multiprocessing file editor 3000, please selelct an option:\n");
-	printf("1) Create a new directorry file\n");
-	printf("2) Create a new regular file\n");
-	printf("3) Create child process to write sorted output\n");
-	printf("4) Create child process to shadow write/read regular files\n");
-	printf("5) Read from a file (print out to stdout)\n");
-	printf("6) Write to a file in either insert, append, or overwrite mode\n");
-	printf("7) Print file status (print out to stdout)\n");
-	printf("8) Print directory listing (contents of dir file) (print out to stdout)\n");
+int isValidCommand(char command){
+	//if the ascii value of the character is between A-H or between a-h 
+	if ( (command >= 65 && command <= 72) || (command >= 97 && command <= 104) ){
+		return 1; //true
+	}
+	return 0; //false
+}
 
-	char command[256];
+void createDirectory(/*char * dirName*/){
+	printf("enter the name:");
+	//validate that the input is correct (doesnt contain '/' or is an empty string)
+
+}
+
+void createFile(/*char * fileName*/){
+	printf("enter the name:");
+}
+
+void readFromFile(){
+	printf("enter the name:");
+}
+
+void writeToFile(/*char * fileName, char * mode*/){
+	printf("enter the name:");
+}
+
+void printFile(){
+	printf("enter the name:"); 
+}
+
+void printDir(){
+	
+}
+
+int main(){
+	printf("Welcome to the super multiprocessing file editron 3000\n");
+	printf("A) Create a new directorry file\n");
+	printf("B) Create a new regular file\n");
+	printf("C) Create child process to write sorted output\n");
+	printf("D) Create child process to shadow write/read regular files\n");
+	printf("E) Read from a file (print out to stdout)\n");
+	printf("F) Write to a file in either insert, append, or overwrite mode\n");
+	printf("G) Print file status (print out to stdout)\n");
+	printf("H) Print directory listing (contents of dir file) (print out to stdout)\n");
+	printf("Please select an option: ");
+	char command;
+
 	// get input from the user and contine only if its valid
-	printf("%s\n", );
-	while (!isValidString()){
-		///please enter a valid string 
+	command = getchar(); ///gets(command,64,stdin);
+	while (!isValidCommand(command)){
+		printf("please enter a character from A to H:");
+		command = getchar();
 	}
 
 	// process the user command
+	switch(command)
+	{
+		case 'a':
+		case 'A': 
+			createDirectory();
+			break;
+		case 'b':
+		case 'B': 
+			createFile();
+			break;
+		case 'c':
+		case 'C': 
 
-	/// switch statement:
-		/// if option 1:
-			///call function that handles oprtion 1:
-	/// same for all the other options
+			break;	
+		case 'd':
+		case 'D': 
+
+			break;
+		case 'e':
+		case 'E': 
+			readFromFile();
+			break;
+		case 'f':
+		case 'F': 
+			writeToFile();
+			break;
+		case 'g':
+		case 'G': 
+			printFileStatus();
+			break;
+		case 'h':
+		case 'H': 
+			printDir();
+			break;
+		default:
+			printf("invalid command");
+	}
 
 
 	return 0;
 }
-
-int isValidString(){
-
-	return 0;
-}
-
-void writeToFile(char * fileName, char * mode){
-}
-
-void createDirectory(char * dirName){
-
-}
-
-void crreateFile(char * fileName)
